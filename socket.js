1e0c7448c7f95e4726c546ef7e777f636359ca1d
bloom510 <bloomjazzer@gmail.com>
2018-06-23 11:26:51 -0700

    organized and cleaned up server code

---
+++ socket.js
@@ -0,0 +1,17 @@
+//Socket.io configuration
+//A bit hard coded for now, can be made more versatile if needed
+class Socket {
+    constructor(server){
+        try{
+        //Create a new HTTP Server to run Socket.io
+        this.server = require('http').Server(server); 
+        //Use that server to run Socket.io
+        this.io = require('socket.io')(this.server); 
+        } catch(e) {
+            console.log(e.stack)
+        }
+    }
+
+}
+
+module.exports = Socket;
