11b78c05c2cabcd7107f8aebdfd8f361f5c2962b
bloom510 <bloomjazzer@gmail.com>
2018-06-24 14:04:04 -0700

    further modularization of Socket class

--- server/socket.js
+++ server/socket.js
@@ -1,20 +1,21 @@
 //Socket.io configuration is a bit hard coded for now, can be made more versatile if needed
 class Socket {
     constructor(app, PORT){
-        //Create a new HTTP Server to run Socket.io
         this.server = require('http').createServer(app); 
-        //Use that server to run Socket.io
         this.io = require('socket.io')(this.server) 
-
-        this.server.listen(
-            PORT,
-                () => {
-                console.log(`app listening on port ${PORT}`)
-                this.io.on('connection', (socket) => { 
-                    socket.on('ready', (data) => {
-                    socket.emit('ready', 'hello from the server side!')
-                    console.log(data)
-                })
+        this.init(PORT)
+    }
+    init(PORT){
+        this.server.listen(PORT, () => {
+            console.log(`app listening on port ${PORT}`)
+            this.listen()
+        })
+    }
+    listen(){
+        this.io.on('connection', (socket) => { 
+            socket.on('ready', (data) => {
+            socket.emit('ready', 'hello from the server side!')
+            console.log(data)
             })
         })
     }
