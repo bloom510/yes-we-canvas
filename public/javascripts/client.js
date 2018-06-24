042d1adc3b54adefd1aa8c583f0bd2aaf115cdaa
bloom510 <bloomjazzer@gmail.com>
2018-06-24 13:56:28 -0700

    solidified bidirectional schema with sockets

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,6 @@
 const socket = io.connect(window.location.host);
 
-socket.on('connect', (data) => {
-    console.log('hello from server')
-    socket.emit('connection')
+socket.on('connect', () => { //when a server connection is established
+    socket.emit('ready', 'hello from the client side!')
+    socket.on('ready', (data) => console.log(data))
 });
--- server/socket.js
+++ server/socket.js
@@ -1,25 +1,22 @@
 //Socket.io configuration is a bit hard coded for now, can be made more versatile if needed
 class Socket {
     constructor(app, PORT){
-        try{
         //Create a new HTTP Server to run Socket.io
         this.server = require('http').createServer(app); 
         //Use that server to run Socket.io
         this.io = require('socket.io')(this.server) 
 
-        this.server.listen(PORT, () => {
-            console.log(`app listening on port ${PORT}`)
-            this.io.emit('connect');
-            this.io.on('connection', () => {
-                console.log('client connected')
-                //event handlers go here
+        this.server.listen(
+            PORT,
+                () => {
+                console.log(`app listening on port ${PORT}`)
+                this.io.on('connection', (socket) => { 
+                    socket.on('ready', (data) => {
+                    socket.emit('ready', 'hello from the server side!')
+                    console.log(data)
+                })
             })
-
-          })
-
-        } catch(e) {
-            console.log(e.stack)
-        }
+        })
     }
 
 }
